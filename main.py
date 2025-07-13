2c522d47d5fb584ca0cb21d4c948aa4289e90007
Pallin1 <mcilwraithdave@gmail.com>
2025-07-13 11:22:38 +0100

    Add player spawn and control, asteroid spawn, collision between players, asteroids and shots, asteroid splitting

--- main.py
+++ main.py
@@ -1,20 +1,66 @@
 import pygame
+import sys
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+from shot import Shot
+
 
 def main():
     pygame.init
+    clock = pygame.time.Clock()
+    dt = 0 #(s) Delta time
+    fps = 60
+
     print("Starting Asteroids!")
     screen = pygame.display.set_mode((SCREEN_WIDTH,SCREEN_HEIGHT))
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    Shot.containers = (shots,updatable,drawable)
+
+    player = Player(x,y)
+    asteroid_field = AsteroidField()
 
     # Game loop
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        
+        updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collision(player):
+                print("Game over!")
+                sys.exit()
+        for asteroid in asteroids:
+            for shot in shots:
+                if shot.collision(asteroid):
+                    shot.kill()
+                    asteroid.split()
+        
+        # Rendering
         screen.fill("black")
+        for entity in drawable:
+            entity.draw(screen)
         pygame.display.flip()
+
+
+        dt_ms = clock.tick(fps)
+        dt = dt_ms/1000
 
 if __name__ == "__main__":
     main()
